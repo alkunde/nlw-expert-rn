775aaed4217e434ed75ddab1deefb58cbb43eda4
André Kunde <andrelbkunde@gmail.com>
2024-02-08 10:28:07 -0300

    third class

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { useRef, useState } from 'react';
 import { FlatList, SectionList, Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
-import { CATEGORIES, MENU } from '@/utils/data/products';
+import { CATEGORIES, MENU, ProductProps } from '@/utils/data/products';
 import { Header } from '@/components/header';
 import { CategoryButton } from '@/components/category-button';
 import { Product } from '@/components/product';
@@ -12,7 +12,7 @@ export default function Home() {
   const cartStore = useCartStore();
   const [category, setCategory] = useState(CATEGORIES[0]);
 
-  const sectionListRef = useRef<SectionList>(null);
+  const sectionListRef = useRef<SectionList<ProductProps>>(null);
 
   const cartQuantityItems = cartStore.products.reduce((total, product) => total + product.quantity, 0);
 
